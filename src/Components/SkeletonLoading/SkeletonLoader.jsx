f68bed98fe07d006ae846f56686eac67c7fb3131
= <abhisingh30800@gmail.com>
2023-05-19 15:22:02 +0530

    added skeleton count for different sizes

--- src/Components/SkeletonLoading/SkeletonLoader.jsx
+++ src/Components/SkeletonLoading/SkeletonLoader.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import SkeletonComp from "./SkeletonComp";
+import Resize from "../../../Resize";
 
 const SkeletonLoader = () => {
+  const windowSize = Resize().size;
+  let skeletonCount =
+    windowSize >= 700
+      ? windowSize >= 1100
+        ? windowSize >= 1400
+          ? 8
+          : 6
+        : 6
+      : 4;
   return (
     <div className="movie--search">
       {Array(6)
